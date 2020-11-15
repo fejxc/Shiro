ccd3e6905fd23a283498aad209e1f74fae248e9a
fejxc <1031364436@qq.com>
2020-11-15 17:40:19 +0800

    init springboot整合shiro

---
+++ ch002-springboot-jsp-shiro/src/main/java/com/sy/Ch002SpringbootJspShiroApplication.java
@@ -0,0 +1,13 @@
+package com.sy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch002SpringbootJspShiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch002SpringbootJspShiroApplication.class, args);
+	}
+
+}
